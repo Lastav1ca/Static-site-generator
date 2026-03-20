b8ef1a1c9f43f246968a80b530cc6cbc951747f9
Lastav1ca <stefandmd22@gmail.com>
2026-03-20 18:31:50 +0100

    Dodati testovi za TextNode

---
+++ src/test_textnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_text_type(self):
+        node = TextNode("Hello", TextType.BOLD)
+        node2 = TextNode("Hello", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, 'mojsajt.com')
+        node2 = TextNode("This is a text node", TextType.BOLD, 'tvojsajt.com')
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_url_one_is_None(self):
+        node = TextNode("This is a text node", TextType.BOLD, 'mojsajt.com')
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
