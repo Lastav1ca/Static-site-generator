9861f1157635ca1366ef6caaf5b4930cef4152ed
Lastav1ca <stefandmd22@gmail.com>
2026-03-21 16:14:48 +0100

    Implementiran ParentNode zajedno sa testom

---
+++ src/parentnode.py
@@ -0,0 +1,23 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError
+        
+        if not self.children:
+            raise ValueError
+        
+        res = f"<{self.tag}>"
+
+        for child in self.children:
+            res += child.to_html()
+
+        res += f"</{self.tag}>"
+
+        return res
+        
+    
