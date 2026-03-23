6d4ca366c329c3b3e16cfa7ad9be9ab689647769
Lastav1ca <stefandmd22@gmail.com>
2026-03-24 00:06:06 +0100

    Implementirana funkcija za splitovanje linije na Nodeove

---
+++ src/split_nodes.py
@@ -0,0 +1,27 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        if delimiter not in node.text:
+            raise ValueError("Delimiter nije pronađen u stringu")
+        
+        node_text = node.text.split(delimiter)
+
+        new_node = []
+
+        new_node.append(TextNode(node_text[0], TextType.TEXT))
+        new_node.append(TextNode(node_text[1], TextType.CODE))
+        new_node.append(TextNode(node_text[2], TextType.TEXT))
+
+        new_nodes.extend(new_node)
+
+    return new_nodes
+
+
+                
